f7679d75bd10e20f2f3f2a0afd84a5fe8ce982b7
Yeinalee <yeinalee@gmail.com>
2024-02-18 18:29:31 +0900

    safe

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from join.views import signup, login
+from server.views import home, goal, subgoal
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", home, name="home"),
     path("join/", signup, name='signup'),
     path("login/", login, name='login'),
+    path('goal/', goal, name="goal"),
+    path('subgoal/', subgoal, name="subgoal"),
 ]
