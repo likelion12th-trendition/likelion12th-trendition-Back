4879b64d6df144823b00b13fa834074aca8feec2
jungbug <xbox1111@naver.com>
2024-02-21 15:26:31 +0900

    fix: allow_host ip

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["52.79.221.189", "localhost", "127.0.0.1"]
 
 
 # Application definition
