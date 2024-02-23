f85d4f9c7f37a1aea4ab2fc920ccc08ae7269e8f
Yeinalee <yeinalee@gmail.com>
2024-02-23 13:19:05 +0900

    username으로 불러오기

--- server/forms.py
+++ server/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from .models import Goal, SubGoal
+from django.forms import formset_factory
 
 class GoalForm(forms.ModelForm):
     class Meta:
         model = Goal
         fields = ['title']
+
+GoalFormSet = formset_factory(GoalForm, max_num=6)
 
 class SubGoalForm(forms.ModelForm):
     class Meta:
