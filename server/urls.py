02806b447d25531ec2d36d368ac313d238dc1267
Yeinalee <yeinalee@gmail.com>
2024-02-23 11:14:54 +0900

    FEAT 새로운 기능 추가1. header에 user token 넣어서 요청 보내시면 완료된 모든 subgoal 불러오기 2. goal_id url로 요청하시면 해당 goal과 연관된 모든 subgoal 불러오기 3. username url로 요청 보내시면 해당 user와 관련된 모든 goal 불러오

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from join.views import *
 from django.conf import settings
 from .views import home, create_goal, update_goal, delete_goal, create_subgoal, update_subgoal, delete_subgoal
+from .views import completed_subgoals, subgoals_by_goal, goals_by_username
 
 urlpatterns = [
     path('', home),
@@ -12,4 +13,7 @@ urlpatterns = [
     path("subgoal/create/<int:goal_id>", create_subgoal),
     path("subgoal/update/<int:subgoal_id>", update_subgoal),
     path('subgoal/delete/<int:subgoal_id>', delete_subgoal),
+    path('subgoal/completed/', completed_subgoals),
+    path('subgoal/bygaol/<int:goal_id>', subgoals_by_goal),
+    path('goal/<str:username>/', goals_by_username),
 ]
